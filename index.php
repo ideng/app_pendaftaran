8006780a3a73ff6ec77e5fc293d3fc576bcebbbd
Ideng Dewo <ideng.dewoadit@gmail.com>
2019-05-19 16:33:42 +0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    $tmp_folder = 'templates/Admin_templates/';
+    include $tmp_folder.'head.php';
+    include $tmp_folder.'body.php';
+    include $tmp_folder.'navbar.php';
+    include $tmp_folder.'breadcrumbs.php';
+    include $tmp_folder.'sidebar.php';
+    include $tmp_folder.'routes.php';
+    include $tmp_folder.'footer.php';
+?>
---
+++ templates/Admin_templates/body.php
@@ -0,0 +1,11 @@
+<body class="no-skin">
+         <div class="main-container ace-save-state" id="main-container">
+            <script type="text/javascript">
+                try{ace.settings.loadState('main-container')}catch(e){}
+            </script>
+
+            <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+                <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
+            </a>
+        </div><!-- /.main-container -->
+</body>
